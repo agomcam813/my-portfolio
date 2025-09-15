fee5db470cc11df4e54f62a04216c5033dd4b2ba
Antonio <antoniogomezcamarena2004@gmail.com>
2025-09-15 16:29:05 +0200

    diseño con datos de ejemplo (midudev)

---
+++ tailwind.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        accent: 'rgb(var(--accent) / <alpha-value>)',
+        'accent-light': 'rgb(var(--accent-light) / <alpha-value>)',
+        'accent-dark': 'rgb(var(--accent-dark) / <alpha-value>)',
+      },
+      backgroundImage: {
+        'accent-gradient': 'var(--accent-gradient)',
+      },
+    },
+  },
+  plugins: [],
+}
